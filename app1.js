ba845fdfd3bf2b7bb50293645fcffccb22ff59fe
mayusuf <samba.yusuf@gmail.com>
2024-08-12 16:52:35 -0500

    login api added

--- app1.js
+++ app1.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const db = require('./dbConfig');
 
 // Create the Express application
@@ -44,22 +45,21 @@ app.get('/api/users', async (req, res) => {
 app.get('/api/users/:userid', async (req, res) => {
   const { userid } = req.params;
 
-  
-    const sql = 'SELECT * FROM tbusers WHERE userid = ?';
-    const [rows] = await db.query(sql, [userid]);
 
-    if (rows.length === 0) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+  const sql = 'SELECT * FROM tbusers WHERE userid = ?';
+  const [rows] = await db.query(sql, [userid]);
 
-    res.status(200).json({
-      message: 'User fetched successfully',
-      data: rows[0]
-    });
+  if (rows.length === 0) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.status(200).json({
+    message: 'User fetched successfully',
+    data: rows[0]
+  });
 });
 
 // API endpoint to handle user data with image 
-
 app.post('/api/user', upload.single('imagelink'), async (req, res) => {
 
   const userid = req.body.userid;
@@ -92,7 +92,6 @@ app.post('/api/user', upload.single('imagelink'), async (req, res) => {
     }
   });
 });
-
 
 
 // PUT route to edit user information
@@ -134,52 +133,83 @@ app.put('/api/user', upload.single('imagelink'), async (req, res) => {
 });
 
 
-// API endpoint to handle form data with image
-app.post('/upload', upload.single('pic'), async (req, res) => {
+// API endpoint to handle user login
+app.post('/api/login', async (req, res) => {
 
-  const name = req.body.name;
-  const image = req.file;
+  const { userid, password } = req.body;
 
-  if (!image) {
-    return res.status(400).json({ message: 'No image uploaded' });
-  }
+  const sql = 'SELECT * FROM tbusers WHERE userid = ?';
+  console.log(sql)
+  const [result] = await db.query(sql, [userid]);
+  // console.log(result[0].password)
 
-  // const imagePath = image.path;
-  const imageFilename = image.filename;
+  // Compare the plain-text password with the hashed password
+  bcrypt.compare(password, result[0].password, (err, isMatch) => {
+    if (err) {
+      return res.status(500).json({ message: 'Error comparing passwords', error: err });
+    }
 
-  // Insert data into the database
-  const sql = 'INSERT INTO profile (name, pic) VALUES (?, ?)';
-  // console.log(sql)
-  // db.query(sql, [name, imageFilename]);
-
-  const [result] = await db.query(sql, [name, imageFilename]);
-
-  // Respond with success message
-  res.status(200).json({
-    message: 'Data saved successfully!',
-    data: {
-      id: result.insertId,
-      name: name,
-      filename: imageFilename
+    if (isMatch) {
+    
+      return res.status(200).json({ 
+        message: 'Login successful', 
+        data: {
+          userid:result[0].userid
+        } 
+      });
+    } else {
+      // Passwords do not match
+      return res.status(401).json({ message: 'Invalid user or password' });
     }
   });
-
-  // , (err, result) => {
-  //   if (err) {
-  //     console.error('Error inserting data into the database:', err);
-  //     return res.status(500).json({ message: 'Database error', error: err });
-  //   }
-
-  //   res.status(200).json({
-  //     message: 'Form data and image uploaded successfully!',
-  //     data: {
-  //       name: name,
-  //       imageFilename: imageFilename
-  //     }
-  //   });
-  // });
-
 });
+
+// API endpoint to handle form data with image
+// app.post('/upload', upload.single('pic'), async (req, res) => {
+
+//   const name = req.body.name;
+//   const image = req.file;
+
+//   if (!image) {
+//     return res.status(400).json({ message: 'No image uploaded' });
+//   }
+
+//   // const imagePath = image.path;
+//   const imageFilename = image.filename;
+
+//   // Insert data into the database
+//   const sql = 'INSERT INTO profile (name, pic) VALUES (?, ?)';
+//   // console.log(sql)
+//   // db.query(sql, [name, imageFilename]);
+
+//   const [result] = await db.query(sql, [name, imageFilename]);
+
+//   // Respond with success message
+//   res.status(200).json({
+//     message: 'Data saved successfully!',
+//     data: {
+//       id: result.insertId,
+//       name: name,
+//       filename: imageFilename
+//     }
+//   });
+
+// , (err, result) => {
+//   if (err) {
+//     console.error('Error inserting data into the database:', err);
+//     return res.status(500).json({ message: 'Database error', error: err });
+//   }
+
+//   res.status(200).json({
+//     message: 'Form data and image uploaded successfully!',
+//     data: {
+//       name: name,
+//       imageFilename: imageFilename
+//     }
+//   });
+// });
+
+// });
 
 
 app.listen(port, () => {
